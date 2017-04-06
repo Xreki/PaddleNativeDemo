934fe5edac207eb2f6d2c79ee476bae9ee713998
Liu Yiqun <liuyiqun01@baidu.com>
2017-04-06 20:20:52 +0800

    Try to link external paddle shared library.

--- android/image_classifier/src/main/cpp/ImageClassifier_jni.cpp
+++ android/image_classifier/src/main/cpp/ImageClassifier_jni.cpp
@@ -1,11 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <paddle/capi.h>
 
 extern "C"
 jstring
-Java_org_paddle_demo_PaddleDemoActivity_stringFromJNI(
+Java_org_paddle_demo_ImageClassifierActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from Paddle";
     return env->NewStringUTF(hello.c_str());
 }
--- android/image_classifier/src/main/java/org/paddle/demo/ImageClassifierActivity.java
+++ android/image_classifier/src/main/java/org/paddle/demo/ImageClassifierActivity.java
@@ -14,7 +14,7 @@ public class ImageClassifierActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("image_classifier");
     }
 
     @Override
